e30410b4ce4ebd8be290f69e3c4dfc95b7d15274
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-17 19:47:35 +0200

    Initial Commit

---
+++ Konwerter/KonwerterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonwerterApp.swift
+//  Konwerter
+//
+//  Created by Natalia Nikiforuk on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct KonwerterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
