130738a5ff31eae2a3d53df86ff6ba9055803535
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-17 22:31:09 +0200

    Basic calculation

--- Konwerter/ContentView.swift
+++ Konwerter/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 0
+    @State private var result = 0
+    @State private var selectedFrom = "h"
+    @State private var selectedTo = "h"
+    
+    let choices = ["h", "min"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Form {
+                Section {
+                    TextField("Value", value: $value, format: .number)
+                        .onChange(of: value) {
+                                    conversion()
+                                }
+                }
+                Section {
+                            Picker("From", selection: $selectedFrom) {
+                                ForEach(choices, id: \.self) {
+                                    Text($0)
+                                }
+                            }
+                            .onChange(of: selectedFrom) {
+                                        conversion()
+                                    }
+                            Picker("To", selection: $selectedTo) {
+                                ForEach(choices, id: \.self) {
+                                    Text($0)
+                                }
+                            }
+                            .onChange(of: selectedTo) {
+                                        conversion()
+                                    }
+                }
+                Section {
+                    Text("The result is: \(result)")
+                        .bold()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func conversion() {
+        if selectedFrom == "h" {
+            if selectedTo == "min" {
+                result = value * 60
+            } else {
+                result = value
+            }
+        }
+        if selectedFrom == "min" {
+            if selectedTo == "h" {
+                result = value / 60
+            } else {
+                result = value
+            }
+        }
     }
 }
 
